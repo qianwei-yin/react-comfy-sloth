8e03885be5052b952cb8ba5e50638aa1a57d6db6
Conway <937700753@qq.com>
2023-01-02 23:45:15 -0800

    finish sort, products list

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,29 +1,54 @@
-import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/filter_reducer'
+import React, { useEffect, useContext, useReducer } from 'react';
+import reducer from '../reducers/filter_reducer';
 import {
-  LOAD_PRODUCTS,
-  SET_GRIDVIEW,
-  SET_LISTVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
-} from '../actions'
-import { useProductsContext } from './products_context'
+	LOAD_PRODUCTS,
+	SET_GRIDVIEW,
+	SET_LISTVIEW,
+	UPDATE_SORT,
+	SORT_PRODUCTS,
+	UPDATE_FILTERS,
+	FILTER_PRODUCTS,
+	CLEAR_FILTERS,
+} from '../actions';
+import { useProductsContext } from './products_context';
 
-const initialState = {}
+const initialState = {
+	filtered_products: [],
+	all_products: [],
+	grid_view: true,
+	sort: 'price-lowest',
+};
 
-const FilterContext = React.createContext()
+const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
-  return (
-    <FilterContext.Provider value='filter context'>
-      {children}
-    </FilterContext.Provider>
-  )
-}
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const { products } = useProductsContext();
+
+	useEffect(() => {
+		dispatch({ type: LOAD_PRODUCTS, payload: products });
+	}, [products]);
+
+	useEffect(() => {
+		dispatch({ type: SORT_PRODUCTS, payload: state.sort });
+	}, [products, state.sort]);
+
+	function setGridView() {
+		dispatch({ type: SET_GRIDVIEW });
+	}
+
+	function setListView() {
+		dispatch({ type: SET_LISTVIEW });
+	}
+
+	function updateSort(e) {
+		const value = e.target.value;
+		dispatch({ type: UPDATE_SORT, payload: value });
+	}
+
+	return <FilterContext.Provider value={{ ...state, setGridView, setListView, updateSort }}>{children}</FilterContext.Provider>;
+};
 // make sure use
 export const useFilterContext = () => {
-  return useContext(FilterContext)
-}
+	return useContext(FilterContext);
+};
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,17 +1,39 @@
 import {
-  LOAD_PRODUCTS,
-  SET_LISTVIEW,
-  SET_GRIDVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
-} from '../actions'
+	LOAD_PRODUCTS,
+	SET_LISTVIEW,
+	SET_GRIDVIEW,
+	UPDATE_SORT,
+	SORT_PRODUCTS,
+	UPDATE_FILTERS,
+	FILTER_PRODUCTS,
+	CLEAR_FILTERS,
+} from '../actions';
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+	switch (action.type) {
+		case LOAD_PRODUCTS:
+			return { ...state, all_products: [...action.payload], filtered_products: [...action.payload] };
+		case SET_GRIDVIEW:
+			return { ...state, grid_view: true };
+		case SET_LISTVIEW:
+			return { ...state, grid_view: false };
+		case UPDATE_SORT:
+			return { ...state, sort: action.payload };
+		case SORT_PRODUCTS:
+			let tempProducts = [...state.filtered_products];
+			if (action.payload === 'price-lowest') {
+				tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+			} else if (action.payload === 'price-highest') {
+				tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+			} else if (action.payload === 'name-a') {
+				tempProducts = tempProducts.sort((a, b) => a.name.localeCompare(b.name));
+			} else if (action.payload === 'name-z') {
+				tempProducts = tempProducts.sort((a, b) => b.name.localeCompare(a.name));
+			}
+			return { ...state, filtered_products: tempProducts };
+		default:
+			throw new Error(`No Matching "${action.type}" - action type`);
+	}
+};
 
-export default filter_reducer
+export default filter_reducer;
